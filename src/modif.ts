2f3ed539656f5000edb98cf70ad8e926c25b37f7
Luciano Sekulic <alu0101022239@ull.edu.es>
2022-04-21 10:12:43 +0000

    subiendo modif

---
+++ src/modif.ts
@@ -0,0 +1,85 @@
+
+/**
+ * clase abstracta a modo de template
+ */
+export abstract class metodoTemplate {
+    protected arrayInicial: number[];
+    protected num: number;
+    constructor() {}
+
+
+    public run() {
+        this.operationFilter(this.arrayInicial, this.num);
+        this.operationMap(this.arrayInicial);
+        this.operationReduce();
+    }
+
+    /**
+     * operacion filtro incial donde se le pasa un array y un numero
+     * en modo de filtro para se quede con los mayores que ese numero
+     * @param arrayInicial 
+     * @param numFiltro 
+     * @returns arrayAux
+     */
+    protected operationFilter(arrayInicial: number[], numFiltro: number) {
+        let arrayAux: number[];
+        for(let i = 0; i < arrayInicial.length; i++) {
+            if(arrayInicial[i] > numFiltro) {
+                arrayAux.push(arrayInicial[i])
+            }
+        }
+        return arrayAux
+    }
+
+    /**
+     * funcion donde se le pasa un array y hace una operacion map inicial
+     * donde se eleva al cuadrado todas las posiciones del arrayInicial
+     * @param arrayInicial 
+     * @returns arrayAuxMap
+     */
+    protected operationMap(arrayInicial: number[]) {
+        let arrayAuxMap: number[];
+        for(let i = 0; i < arrayInicial.length; i++) {
+            arrayAuxMap.push(Math.pow(arrayInicial[i], 2))
+        }
+        return arrayAuxMap
+    }
+
+    protected operationReduce(){
+
+    }
+
+}
+
+
+/**
+ * clase de algoritmos genericos de filter, map y reduce
+ */
+export class algoritmosGenericos extends metodoTemplate{
+    constructor(protected arrayGenerico: number[], protected numFiltro: number){
+        super();
+    }
+
+    protected filter(arrayGenerico: number[], operationFilter: (numFiltro: number) => number) {
+        return operationFilter
+    }
+    protected Map(arrayGenerico: number[], operationMap: (arrayGenerico: number[]) => number) {
+        return operationMap
+    }
+
+    /**
+     * en reduce se realiza la suma de los numeros dentro del array
+     * @param arrayAuxMap 
+     * @returns aux
+     */
+    protected reduce(arrayAuxMap: number[]) {
+        let aux: number = 0;
+        for(let i = 0; i < arrayAuxMap.length; i++) {
+            aux = aux + arrayAuxMap[i];
+        }
+        return aux
+    }
+
+
+
+}
---
+++ tests/modif.spec.ts
@@ -0,0 +1,24 @@
+import 'mocha';
+import { expect } from 'chai';
+import { algoritmosGenericos } from '../src/modif';
+
+describe('prueba modif', () =>{
+  let numero: number = 2
+  let numeroFiltro: number = 2
+  let array: number[] = [1, 4, 6, 8, 10];
+  let algoritm = new algoritmosGenericos([1, 4, 6, 8, 10], 2);
+  let arrayResultFilter: number[] = [6, 8, 10]
+  let arrayResultMap: number[] = [1, 16, 36, 64, 100]
+
+  it('test de filter', () => {
+    expect(algoritm.run(functions).to.be.deep.equal(arrayResultFilter);
+  });
+
+  it('test de map', () => {
+    expect(array.map(array)).to.be.deep.equal(arrayResultMap);
+  });
+
+  it('test de reduce', () => {
+    expect(array.reduce(array)).to.be.equal(29);
+  });
+})
